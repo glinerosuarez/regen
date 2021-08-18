004e257690fbf5c0b9c4880e097e7370054259f3
Gabriel Linero <gllinero@ch-nb-4850.uptake.com>
2021-08-18 00:00:24 -0500

    create constants for BNB client

--- consts.py
+++ consts.py
@@ -1,8 +1,6 @@
 import enum
-from collections import namedtuple
-from dataclasses import dataclass
-
 from attr import attrs, attrib
+from dataclasses import dataclass
 from attr.validators import instance_of
 
 
@@ -19,6 +17,36 @@ class EnvConsts:
     MAX_STEPS = 20000
 
     LOOKBACK_WINDOW_SIZE = 40
+
+
+class Side(enum.Enum):
+    """whether you want to BUY or SELL a base."""
+    BUY: str = 'BUY'
+    SELL: str = 'SELL'
+
+
+class OrderType(enum.Enum):
+    """the type of order you want to submit."""
+    LIMIT: str = 'LIMIT'
+    MARKET: str = 'MARKET'
+    STOP_LOSS: str = 'STOP_LOSS'
+    STOP_LOSS_LIMIT: str = 'STOP_LOSS_LIMIT'
+    TAKE_PROFIT: str = 'TAKE_PROFIT'
+    TAKE_PROFIT_LIMIT: str = 'TAKE_PROFIT_LIMIT'
+    LIMIT_MAKER: str = 'LIMIT_MAKER'
+
+
+class TimeInForce(enum.Enum):
+    """Expresses how you want the order to execute."""
+    # (good till canceled) – perhaps the most popular setup, GTC will ensure that your order is valid until it’s filled,
+    # or until you cancel it.
+    GTC: str = 'GTC'
+    # (fill or kill) – FOK instructs the exchange to execute an order all at once. If the exchange can’t do so, the
+    # order is immediately canceled.
+    FOK: str = 'FOK'
+    # (immediate or cancel) – either all or part of the order must be executed immediately, or it’s canceled. Unlike
+    # FOK, the orders are not canceled if they can be partially filled.
+    IOC: str = 'IOC'
 
 
 class CryptoAsset(enum.Enum):
