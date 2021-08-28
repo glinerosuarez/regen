3f09b1d3c407129a3f43da8ed7c1599d14a9006f
Gabriel Linero <gllinero@tr-nb-4992.uptake.com>
2021-08-28 16:42:55 -0500

    utils module

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from enum import Enum
 from typing import Dict, Any
 
@@ -13,3 +14,21 @@ def remove_none_args(args: Dict[str, Any]) -> Dict[str, Any]:
         for k, v in args.items()
         if v is not None
     }
+
+
+def datetime_to_ts_ms(dt: datetime) -> int:
+    """
+    Convert datetime to timestamp in ms.
+    :param dt: datetime
+    :return: timestamp in ms
+    """
+    return int(datetime.timestamp(dt)) * 1000
+
+
+def ts_ms_to_datetime(ts: int) -> datetime:
+    """
+    Convert timestamp in ms to datetime
+    :param ts: timestamp in ms
+    :return: datetime
+    """
+    return datetime.fromtimestamp(int(ts / 1000))
