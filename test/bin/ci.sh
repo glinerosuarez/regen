f123e1e190ad1dcbc76b23c86b27ea8eafd63c11
Gabriel Linero <c.gabriel.linero@uptake.com>
2022-04-18 19:13:37 -0500

    exclude E203 from linter

--- test/bin/ci.sh
+++ test/bin/ci.sh
@@ -2,9 +2,13 @@
 
 set -e
 
+# Formatter
+pip install black
+black -l 120 -t py310 --check .
+
 # Linter
 pip install flake8
-flake8 . --max-line-length=120 --exclude=test,__init__.py
+flake8 . --max-line-length=120 --exclude=test,__init__.py --ignore=E203
 
 # Tests
 pip install pytest
