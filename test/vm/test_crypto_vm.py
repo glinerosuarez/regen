11f17e68cc32d9908ed69dd375fa907c22f252af
Gabriel Linero <c.gabriel.linero@uptake.com>
2022-04-19 22:49:42 -0500

    crypto vm producing klines per min

---
+++ test/vm/test_crypto_vm.py
@@ -0,0 +1,21 @@
+import time
+
+from consts import CryptoAsset
+from vm.crypto_vm import CryptoViewModel
+
+
+def test_crypto_vm():
+    vm = CryptoViewModel(CryptoAsset.BNB, CryptoAsset.BUSD, 5)
+    # Start the producer
+    print("Starting producer")
+    vm.reset()
+    print("I'm able to keep doing stuff in this thread")
+    time.sleep(5)
+    print("Oh yeah after 5 secs I'm still able to do other stuff!")
+    print(vm.get_observation())
+
+    print("It's over my friend")
+
+
+if __name__ == '__main__':
+    test_crypto_vm()
