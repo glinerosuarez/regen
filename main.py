c859ede02fcb2811be9af84f3a98041ef190ed26
Gabriel Linero <c.gabriel.linero@uptake.com>
2022-07-16 16:31:44 -0500

    got training working

--- main.py
+++ main.py
@@ -10,22 +10,24 @@ from env import CryptoTradingEnv
 
 
 def train():
-    timesteps = 5
+    time_steps = 8
+    base_asset = CryptoAsset.BNB
+    quote_asset = CryptoAsset.BUSD
 
-    env = CryptoTradingEnv(window_size=5, base_asset=CryptoAsset.BNB, quote_asset=CryptoAsset.BUSD, base_balance=100)
+    env = CryptoTradingEnv(window_size=5, base_asset=base_asset, quote_asset=quote_asset, base_balance=100)
     env = make_vec_env(lambda: env, n_envs=1)
 
     # set up logger
     tmp_path = "output/logs/"
     new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
 
-    model = A2C(MlpPolicy, env, verbose=1)
+    model = PPO(MlpPolicy, env, verbose=1, n_steps=time_steps, batch_size=time_steps)
     model.set_logger(new_logger)
-    model.learn(total_timesteps=timesteps)
+    model.learn(total_timesteps=time_steps)
 
     ts = datetime.today()
     model.save(
-        f"output/models/PPO_{env.base_asset.name}{env.quote_asset.name}_{timesteps}_{ts.year}{ts.month}{ts.day}"
+        f"output/models/PPO_{base_asset.name}{quote_asset.name}_{time_steps}_{ts.year}{ts.month}{ts.day}"
         f"{ts.hour}{ts.minute}{ts.second}"
     )
 
