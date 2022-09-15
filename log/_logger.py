50e2bc763a377e79a97271e8ea3e6ea60b457655
Gabriel Linero <c.gabriel.linero@uptake.com>
2022-09-15 18:29:48 -0500

    bump logs severity

--- log/_logger.py
+++ log/_logger.py
@@ -67,16 +67,16 @@ class LoggerFactory:
             return logger
         else:
             # Set severity level.
-            logger.setLevel(logging.DEBUG)
+            logger.setLevel(logging.INFO)
 
             # Create file stderr handler.
             err_handler = logging.FileHandler("output/logs/" + filename + "_stderr.log")
-            err_handler.setLevel(logging.DEBUG)
+            err_handler.setLevel(logging.INFO)
             err_handler.addFilter(StdErrFilter())
 
             # Create stdout handler.
             out_handler = logging.FileHandler("output/logs/" + filename + "_stdout.log")
-            out_handler.setLevel(logging.DEBUG)
+            out_handler.setLevel(logging.INFO)
             out_handler.addFilter(StdOutFilter())
 
             # Create formatter.
