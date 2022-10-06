defc59ceef1fc4f2187212dc4dc42d39a5bad0c7
Gabriel Linero <c.gabriel.linero@uptake.com>
2022-10-05 21:11:41 -0500

    monkeypatch random.uniform

--- test/env/test_obs.py
+++ test/env/test_obs.py
@@ -1,3 +1,4 @@
+import random
 from typing import List
 
 import numpy as np
@@ -7,8 +8,13 @@ from test.env.utils import normalize_obs
 
 
 @pytest.fixture
-def trade_prices(klines_data, window_size) -> List[float]:
-    tp = [23.589216190521284]
+def init_price() -> float:
+    return 23.589216190521284
+
+
+@pytest.fixture
+def trade_prices(klines_data, window_size, init_price) -> List[float]:
+    tp = [init_price]
     update_tp = [i + window_size for i in [0, 4, 6, 7, 11, 13]]
     for i in range(window_size, len(klines_data)):
         if i in update_tp:
@@ -37,7 +43,9 @@ def expected_obs(klines_data, window_size, trade_prices) -> List[np.ndarray]:
     return normalize_obs(obs)
 
 
-def test_normalized_obs(insert_klines, env, expected_obs, actions):
+def test_normalized_obs(monkeypatch, insert_klines, init_price, env, expected_obs, actions):
+    monkeypatch.setattr(random, "uniform", lambda l_bound, u_bound: init_price)
+
     obs = env.reset()
     assert np.array_equal(obs["klines"], expected_obs[0])
 
