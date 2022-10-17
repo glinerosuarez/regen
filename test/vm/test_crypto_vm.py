ebbbb3bd33aaf9d69baf0d2f9ffe3eab7b841114
Gabriel Linero <c.gabriel.linero@uptake.com>
2022-10-16 20:29:43 -0500

    test last price

--- test/vm/test_crypto_vm.py
+++ test/vm/test_crypto_vm.py
@@ -1,29 +1,13 @@
-"""
-import time
+def test_last_trade_price_between_episodes(insert_klines_2ep, vm, actions, window_size):
+    vm.reset()
+    for i, a in enumerate(actions):
+        obs, reward, done, _ = vm.step(a)
+        if done:
+            last_price = vm.last_price
+            last_trade_price = vm.last_trade_price
+            break
 
-import configuration
-from consts import CryptoAsset
-from repository.db import DataBaseManager, Kline
-from consts import Action
-from vm.crypto_vm import CryptoViewModel
-
-
-def test_crypto_vm():
-    DataBaseManager._engine = None
-    configuration.settings.ticks_per_episode = 3
-    db_manager = DataBaseManager(configuration.settings.db_name)
-
-    print(db_manager.select_all(Kline))
-
-    vm = CryptoViewModel(CryptoAsset.BNB, CryptoAsset.BUSD, 5, 100)
-    # Start the producer
-    print("Starting producer")
-    print(f"first obs: {vm.reset()}")
-
-    print("I'm able to keep doing stuff in this thread")
-    time.sleep(5)
-    print("Oh yeah after 5 secs I'm still able to do other stuff!")
-    print(vm.step(Action.Buy))
-
-    print("It's over my friend")
-"""
+    vm.reset()
+    assert i == 9 - window_size
+    assert last_price == vm.last_price
+    assert last_trade_price == vm.last_trade_price
