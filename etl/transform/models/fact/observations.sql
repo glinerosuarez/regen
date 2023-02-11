7c6dbb843f700657fcf120c083ca80791636955e
Gabriel Linero <c.gabriel.linero@uptake.com>
2023-02-11 13:59:28 -0500

    observation model

--- etl/transform/models/fact/observations.sql
+++ etl/transform/models/fact/observations.sql
@@ -1,7 +1,7 @@
 WITH src_klines AS (
     SELECT * FROM {{ ref('src_klines') }}
-) ma AS (
-    SELECT * FROM {{ ref('stg_ma') }}
+), ma AS (
+    SELECT * FROM {{ ref('src_mas') }}
 )
 SELECT
     sk.kline_id AS kline_id
@@ -11,13 +11,13 @@ SELECT
     , sk.low AS low
     , sk.close_value AS close_value
     --, sk.close_ts AS close_ts
-    , sk.ma_7 AS ma_7
-    , sk.ma_25 AS ma_25
-    , sk.ma_100 AS ma_100
-    , sk.ma_300 AS ma_300
-    , sk.ma_1440 AS ma_1440
-    , sk.ma_14400 AS ma_14400
-    , sk.ma_144000 AS ma_144000
+    , ma.ma_7 AS ma_7
+    , ma.ma_25 AS ma_25
+    , ma.ma_100 AS ma_100
+    , ma.ma_300 AS ma_300
+    , ma.ma_1440 AS ma_1440
+    , ma.ma_14400 AS ma_14400
+    , ma.ma_144000 AS ma_144000
     , sk.open_ts AS open_ts
 FROM
-    src_klines sk INNER JOIN ma ON sk.kline_id = ma,kline_id
+    src_klines sk INNER JOIN ma ON sk.kline_id = ma.kline_id
--- infra/dbt/server.py
+++ infra/dbt/server.py
@@ -1,33 +1,29 @@
 import subprocess
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask("dbt")
 
 
 @app.route("/run")
 def run():
-    result = subprocess.run(["dbt run"], shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return jsonify(
-        {
-            "return_code": result.returncode,
-            "stdout": result.stdout.decode("utf-8"),
-            "stderr": result.stderr.decode("utf-8"),
-        }
-    )
+    result = subprocess.run(["dbt run"], shell=True, check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return {
+        "return_code": result.returncode,
+        "stdout": result.stdout.decode("utf-8"),
+        "stderr": result.stderr.decode("utf-8"),
+    }
 
 
 @app.route("/run_exclude_staging")
 def run_exclude_staging():
     result = subprocess.run(
-        ["dbt run --exclude models/staging/*+"], shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        ["dbt run --exclude models/staging/*+"], shell=True, check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
-    return jsonify(
-        {
-            "return_code": result.returncode,
-            "stdout": result.stdout.decode("utf-8"),
-            "stderr": result.stderr.decode("utf-8"),
-        }
-    )
+    return {
+        "return_code": result.returncode,
+        "stdout": result.stdout.decode("utf-8"),
+        "stderr": result.stderr.decode("utf-8"),
+    }
 
 
 if __name__ == "__main__":
